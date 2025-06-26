9ad119205fd211f881eead65b0bb2ee6104df180
STOK3 <67011392@kmitl.ac.th>
2025-06-26 18:43:33 +0700

    fixed format

---
+++ rush/ex01/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+from checkmate import checkmate
+
+def main():
+
+    board =  """\
+    .R..
+    ..K.
+    ....
+    ....\
+    """
+    checkmate(board)
+
+
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            try:
+                board = open(arg, "r").read()
+                checkmate(board)
+            except:
+                print("Error: file cannot be read")
+
+if __name__ == "__main__":
+    main()
